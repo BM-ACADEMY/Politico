0450a5437d3ad776b9f9c897037fe3f37a139b8e
BM-ACADEMY <teamkamarweb@gmail.com>
2025-09-29 16:26:42 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,13 +11,13 @@ export default defineConfig({
       "@": path.resolve("./src"),
     },
   },
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:5000',
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
